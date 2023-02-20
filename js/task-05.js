816ee972144981a94f2a94c99d967f94338c3f33
Artem <protastema1803@gmail.com>
2023-02-20 21:47:15 +0200

    Виправлення 5 дз

--- js/task-05.js
+++ js/task-05.js
@@ -3,11 +3,14 @@ const refs = {
     nameLabel: document.querySelector("#name-output"),
 };
 
+const anonim = "Anonymous";
 
 refs.name.addEventListener('input', onInputChange);
 
 function onInputChange(event) {
     refs.nameLabel.textContent = event.currentTarget.value;  
-    
+    if (event.currentTarget.value === "") {
+        refs.nameLabel.textContent = anonim;
+    }
 };
 
